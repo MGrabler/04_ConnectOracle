07894e746f50316133521a6150e7c7b5d25a50c5
MGrabler <GAB19925@spengergasse.at>
2021-04-25 16:23:24 +0200

    SerialVersion

--- src/domain/BankAccountException.java
+++ src/domain/BankAccountException.java
@@ -1,12 +1,11 @@
 package domain;
 
-@SuppressWarnings("serial")
 public class BankAccountException extends Exception
 {
-//	/**
-//	 * 
-//	 */
-//	private static final long serialVersionUID = 1L;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	public BankAccountException(String message)
 	{
